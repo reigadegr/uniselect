9d50d35a0021fa24b90534dc1425d531c9824ae8
reigadegr <2722688642@qq.com>
2024-07-29 14:48:08 +0800

    更新依赖 使用hash路由方式防止刷新404

--- uniselect/src/main.js
+++ uniselect/src/main.js
@@ -3,6 +3,6 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 import ElementPlus from "element-plus";
-import "element-plus/theme-chalk/index.css";
+import "element-plus/dist/index.css";
 
 createApp(App).use(store).use(router).use(ElementPlus).mount("#app");
--- uniselect/src/router/index.js
+++ uniselect/src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from "vue-router";
+import {createRouter, createWebHashHistory, createWebHistory} from "vue-router";
 import HomeView from "@/views/HomeView.vue";
 
 const routes = [
@@ -125,7 +125,8 @@ const routes = [
 ];
 
 const router = createRouter({
-  history: createWebHistory(process.env.BASE_URL),
+  // history: createWebHistory(process.env.BASE_URL),
+  history: createWebHashHistory(),
   routes,
 });
 
